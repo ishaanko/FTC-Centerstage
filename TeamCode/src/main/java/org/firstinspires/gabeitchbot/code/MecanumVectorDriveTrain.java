c8e25a1f8fb8acc60f72ef080f552669d711943a
Ishaan Kothari <ishaan@ikothari.com>
2023-12-19 14:49:25 -0800

    Packages

--- TeamCode/src/main/java/org/firstinspires/gabeitchbot/code/MecanumVectorDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/gabeitchbot/code/MecanumVectorDriveTrain.java
@@ -1,4 +1,4 @@
-package org.virus.agobot;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
